0464d3348550ffa118f1675f9bced5885373825d
John Miller <john.miller@unionstreet.co.uk>
2024-09-10 18:02:07 +0100

    Refactor service extensions

--- Microservice.Register.Function/Program.cs
+++ Microservice.Register.Function/Program.cs
@@ -1,22 +1,8 @@
-using Azure.Identity;
-using FluentValidation;
-using MediatR;
-using Microservice.Register.Function.Data.Context;
-using Microservice.Register.Function.Data.Repository;
-using Microservice.Register.Function.Data.Repository.Interfaces;
 using Microservice.Register.Function.Helpers;
-using Microservice.Register.Function.Helpers.Interfaces;
-using Microservice.Register.Function.MediatR.RegisterUser;
 using Microservice.Register.Function.Middleware;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Reflection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(builder => builder.UseMiddleware<ExceptionHandlerMiddleware>())
@@ -30,35 +16,15 @@ var host = new HostBuilder()
         var configuration = services.BuildServiceProvider().GetService<IConfiguration>()
                               ?? throw new Exception("Configuration not created.");
 
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddValidatorsFromAssemblyContaining<RegisterUserValidator>();
-        services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
-        services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IAzureServiceBusHelper, AzureServiceBusHelper>();
-        services.AddScoped<IAuthenticationHelper, AuthenticationHelper>();
-        services.AddScoped<IJsonHelper, JsonHelper>();
-        services.AddSingleton<DefaultData>();
-        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        services.AddMemoryCache();
-        services.AddApplicationInsightsTelemetry();
-        services.AddDbContextFactory<UserDbContext>(options =>
-
-        options.UseSqlServer(configuration.GetConnectionString(Constants.DatabaseConnectionString),
-            options => options.EnableRetryOnFailure()));
-
-        services.AddAzureClients(builder =>
-        {
-            builder.AddServiceBusClientWithNamespace(EnvironmentVariables.GetEnvironmentVariable(Constants.AzureServiceBusConnection));
-            builder.UseCredential(new ManagedIdentityCredential());
-        });
-
-        services.AddLogging(logging =>
-        {
-            logging.AddConsole();
-        });
+        ServiceExtensions.ConfigureApplicationInsights(services);
+        ServiceExtensions.ConfigureMediatr(services);
+        ServiceExtensions.ConfigureDependencyInjection(services);
+        ServiceExtensions.ConfigureMemoryCache(services);
+        ServiceExtensions.ConfigureSqlServer(services, configuration);
+        ServiceExtensions.ConfigureServiceBusClient(services);
+        ServiceExtensions.ConfigureLogging(services);
     })
     .Build();
 
 await host.RunAsync();
+
