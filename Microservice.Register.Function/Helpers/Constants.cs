85a121eb6daa4cee2d7f581eaa7ee72d8c8649b8
John Miller <john.miller@unionstreet.co.uk>
2024-09-10 11:35:56 +0100

    Setup Azure service bus client

--- Microservice.Register.Function/Helpers/Constants.cs
+++ Microservice.Register.Function/Helpers/Constants.cs
@@ -2,7 +2,7 @@
 
 public class Constants
 {
-    public const string AzureServiceBusConnection = "AZURE_SERVICE_BUS_CONNECTION";
+    public const string AzureServiceBusConnection = "ServiceBusConnection__fullyQualifiedNamespace";
     public const string AzureServiceBusQueueRegisteredUserCustomer = "AZURE_SERVICE_BUS_QUEUE_CUSTOMER";
     public const string AzureServiceBusQueueRegisteredUserCustomerAddress = "AZURE_SERVICE_BUS_QUEUE_CUSTOMER_ADDRESS";
     public const string DatabaseConnectionString = "SQLAZURECONNSTR_USER";
