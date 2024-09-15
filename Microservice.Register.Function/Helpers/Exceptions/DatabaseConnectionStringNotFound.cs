de036cc68514fd10c303fdc4d2cba91d4681f4aa
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-15 13:46:35 +0100

    Refactor Sql Server Provider

---
+++ Microservice.Register.Function/Helpers/Exceptions/DatabaseConnectionStringNotFound.cs
@@ -0,0 +1,17 @@
+﻿namespace Microservice.Register.Function.Helpers.Exceptions;
+public class DatabaseConnectionStringNotFound : Exception
+{
+    public DatabaseConnectionStringNotFound()
+    {
+    }
+
+    public DatabaseConnectionStringNotFound(string message)
+        : base(message)
+    {
+    }
+
+    public DatabaseConnectionStringNotFound(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
